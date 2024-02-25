81cbffd6c7907d0044d41f277b407fc407bc1fa4
bipinbura <136945606+bipinbura@users.noreply.github.com>
2024-02-25 23:05:42 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {  } from 'react'
+
+import './App.css'
+import Login from './component/pages/Login/Login'
+
+function App() {
+
+
+  return (
+    <>
+    <Login/>
+    </>
+  )
+}
+
+export default App
